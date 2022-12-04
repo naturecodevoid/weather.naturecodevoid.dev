bae8c975a922f4ee04236145b930f467d175e903
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2022-12-04 13:13:24 -0800

    better deployment

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+bun run build
+
+cd dist
+
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m "deploy"
+
+git push -f git@github.com:naturecodevoid/weather.naturecodevoid.dev.git main:gh-pages
+
+cd -
