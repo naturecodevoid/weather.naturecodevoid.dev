c56ac433ff1f04202fe172d151aa06a3ee9c7b0e
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-02-01 18:20:17 -0800

    round latitude longitude to 3 decimal points in info box and small refactor

--- src/components/InfoBox.svelte
+++ src/components/InfoBox.svelte
@@ -6,13 +6,23 @@
 
     export let lastRefreshed: Dayjs;
 
+    const increment = 0.75;
+    const originalSize = "0.95em";
+
     let box: HTMLDivElement;
     let location: HTMLSpanElement;
 
     let trimmedLatLon = "";
     onMount(() => {
         latLon.subscribe((val) => {
-            trimmedLatLon = val.replace(",", ", ");
+            trimmedLatLon = val
+                .split(",")
+                .map((val) => {
+                    const numVal = Number.parseFloat(val);
+                    if (!isNaN(numVal)) return numVal.toFixed(3);
+                    return val;
+                })
+                .join(", ");
             adjustLocationFontSize();
         });
         placeOutput.subscribe((_) => {
@@ -27,16 +37,16 @@
     function adjustLocationFontSize() {
         if (!location) return console.log("skipping adjustment");
         location.textContent = getLocation();
-        location.style.fontSize = "0.95em";
+        location.style.fontSize = originalSize;
         const boxWidth = box.getBoundingClientRect().width;
         if (location.getBoundingClientRect().width < boxWidth) return console.log("text fits in box");
 
         let currentFontSize = Number.parseFloat(getComputedStyle(location).fontSize);
 
         while (location.getBoundingClientRect().width > boxWidth) {
-            currentFontSize -= 1;
+            currentFontSize -= increment;
             location.style.fontSize = currentFontSize.toString() + "px";
-            if (currentFontSize < 5) break;
+            if (currentFontSize < increment) break;
         }
 
         console.log(`adjusted text to ${location.style.fontSize}`);
@@ -46,7 +56,7 @@
 <div class="location-last-refreshed" style="left: calc(50% - 125px);" bind:this={box}>
     <h4 style="padding-top: 7px">Last refreshed: {lastRefreshed == null ? "Never" : lastRefreshed.format("M/D h:mm:ss A")}</h4>
     <h4>
-        <span style="font-size: 0.95em; width: unset" bind:this={location}>{getLocation()}</span>
+        <span style="font-size: {originalSize}; width: unset" bind:this={location}>{getLocation()}</span>
     </h4>
 </div>
 
